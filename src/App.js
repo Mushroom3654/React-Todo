1841d4a4ecbb25b75ee789bea90e76cc976ddb8d
KBY <dhqoavkdldj@gmail.com>
2019-07-11 22:55:58 +0900

    todo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import {initFirebase} from "./Shared/Firebase";
+import TodoItem from "./Components/TodoItem";
+import Pallete from "./Components/Pallete";
+import TodoInput from "./Components/TodoInput";
+import './Css/Template.css';
+import TodoList from "./Components/TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends  Component{
+  constructor() {
+    super();
+    initFirebase();
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <main className="template">
+            <div className="title">
+              Todo-List
+            </div>
+            <Pallete/>
+            <TodoInput/>
+            <TodoList/>
+          </main>
+        </div>
+    );
+  }
+
 }
 
 export default App;
